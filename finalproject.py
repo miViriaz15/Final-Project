50183466eea93aad3588bf260c07d14291068f7c
miViriaz15 <46974607+miViriaz15@users.noreply.github.com>
2019-04-09 14:51:47 -0400

    Updated from Brython Server: 4/9/2019 2:51:45 PM

--- finalproject.py
+++ finalproject.py
@@ -3,3 +3,11 @@ miViriaz15 Final Project - turtle graphics in ggame
 https://docs.python.org/3.3/library/turtle.html?highlight=turtle
 '''
 
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame, PolygonAsset
+
+class Turtle(Sprite):
+    
+    def __init__(self):
+        super.__init__(centerposition, PolygonAsset())
+    
+    #position, direction, penstate, color
