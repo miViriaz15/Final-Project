cb72f3fd46c6936c701fbabe092313d2bb251517
miViriaz15 <46974607+miViriaz15@users.noreply.github.com>
2019-04-09 15:08:00 -0400

    Updated from Brython Server: 4/9/2019 3:07:58 PM

--- finalproject.py
+++ finalproject.py
@@ -4,7 +4,7 @@ https://docs.python.org/3.3/library/turtle.html?highlight=turtle
 '''
 
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame, PolygonAsset, EllipseAsset, CircleAsset
-
+myapp = App()
 #defining colors
 black = Color(0x000000, 1.0)
 white = Color(0xFFFFFF, 1.0)
@@ -14,9 +14,9 @@ white = Color(0xFFFFFF, 1.0)
 thinlinewhite = LineStyle(1, white)
 thinlineblack = LineStyle(1, black)
 
+Sprite(PolygonAsset([(5,5),(20,12),(5,19),(10,12),(5,5)],thinlineblack, white))
 
-
-
+'''
 class Turtle(Sprite):
     #screencenter
     startturtle=PolygonAsset([(5,5),(25,10),(5,15),(10,10),(5,5)],thinlineblack, white)
@@ -24,3 +24,6 @@ class Turtle(Sprite):
         super().__init__(screencenter, startturtle)
     
     #position, direction, penstate, color
+'''
+
+myapp.run()
