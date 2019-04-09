9fb5466d036383c8d38bfd6bc687fd733df9a2b6
miViriaz15 <46974607+miViriaz15@users.noreply.github.com>
2019-04-09 15:04:57 -0400

    Updated from Brython Server: 4/9/2019 3:04:52 PM

--- finalproject.py
+++ finalproject.py
@@ -3,11 +3,24 @@ miViriaz15 Final Project - turtle graphics in ggame
 https://docs.python.org/3.3/library/turtle.html?highlight=turtle
 '''
 
-from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame, PolygonAsset
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame, PolygonAsset, EllipseAsset, CircleAsset
+
+#defining colors
+black = Color(0x000000, 1.0)
+white = Color(0xFFFFFF, 1.0)
+
+
+#defining line
+thinlinewhite = LineStyle(1, white)
+thinlineblack = LineStyle(1, black)
+
+
+
 
 class Turtle(Sprite):
-    
+    #screencenter
+    startturtle=PolygonAsset([(5,5),(25,10),(5,15),(10,10),(5,5)],thinlineblack, white)
     def __init__(self):
-        super.__init__(centerposition, PolygonAsset())
+        super().__init__(screencenter, startturtle)
     
     #position, direction, penstate, color
