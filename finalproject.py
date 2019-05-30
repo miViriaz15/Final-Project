88df56f1970694ba12ae51f4c30400a413fef72e
miViriaz15 <46974607+miViriaz15@users.noreply.github.com>
2019-05-30 13:53:08 -0400

    Updated from Brython Server: 5/30/2019 1:53:06 PM

--- finalproject.py
+++ finalproject.py
@@ -320,41 +320,6 @@ Screen() # Creates a playground for turtles
 #alex=Turtle()
 
 
-turtle = Turtle()     #Bob is our turtle
-
-turtle.left(20)
-
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-
-turtle.left(30)
-
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-
-turtle.left(40)
-
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-
 
 '''
 distance=20 #spiral
